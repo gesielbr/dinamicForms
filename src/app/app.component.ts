c6d381ea9cb628632cc50492376a8b169932b2a2
gesielbr <gesiel.br@gmail.com>
2024-07-16 19:37:48 -0300

    form Array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  FormArray,
+  FormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'dinamicForms';
+
+  employeeForm: FormGroup;
+
+  constructor() {
+    this.employeeForm = new FormGroup({
+      employees: new FormArray([]),
+    });
+  }
+
+  get employees(): FormArray {
+    return this.employeeForm.get('employees') as FormArray;
+  }
+
+  addEmployee() {
+    const employeeGroup = new FormGroup({
+      name: new FormControl(''),
+      job: new FormControl(''),
+    });
+    this.employees.push(employeeGroup);
+  }
+
+  submitForm() {
+    console.log(this.employeeForm.value);
+  }
 }
